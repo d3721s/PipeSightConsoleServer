f47e62a0d4f4556f2986c3a036d07fa4d8562843
d3721s <981889017@qq.com>
2026-06-13 21:18:20 +0800

    Update vite.config.ts

--- front_end/vite.config.ts
+++ front_end/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': 'http://127.0.0.1:8000',
       '/whep': 'http://127.0.0.1:8000',
+      '/storage': 'http://127.0.0.1:8000',
       '/ws': {
         target: 'ws://127.0.0.1:8000',
         ws: true
